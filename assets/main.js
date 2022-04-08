70d25911b454ccc9db8c4a55bc369f1f5face89f
mipierc <mpierce2602@gmail.com>
2022-04-08 10:44:09 -0400

    styles card

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,20 @@
 // DOM Objects
 const characterName = document.getElementById('char-name');
-const characterID = document.getElementById('id-num');
+const characterID = document.getElementById('char-id');
 const characterStatus = document.getElementById('char-status');
 const characterSpecies = document.getElementById('char-species');
-const characterType = document.getElementById('poke-type');
-const characterGender = document.getElementById('poke-gender');
+const characterType = document.getElementById('char-type');
+const characterGender = document.getElementById('char-gender');
 const characterImage = document.getElementById('front-image');
 
-function getPokemon() {
-  let randomNum = Math.floor(Math.random() * 825 + 1);
-  fetch('https://rickandmortyapi.com/api/character/' + randomNum)
+function getCharacter() {
+  let randomNum = Math.floor(Math.random() * 10 + 1);
+  let x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  fetch(' https://api.coinlore.net/api/ticker/?id=' + x[randomNum])
   .then(res => res.json())
   .then(data => {
-    characterName.innerHTML = data['name'];
+    console.log(data)
+    characterName.innerHTML = data['0']['name'];
     characterID.innerHTML = data['id'];
     characterStatus.innerHTML = data['status'];
     characterSpecies.innerHTML = data['species'];
